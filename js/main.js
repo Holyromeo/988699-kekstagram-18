9d4a0e351be403af68b4c0b475a077108ccf199f
Holyromeo <ramankast@mail.ru>
2019-11-04 23:37:31 +0300

    update debounce

--- js/main.js
+++ js/main.js
@@ -41,14 +41,15 @@
     });
   }
 
-  function clearPictures() {
+  var clearPictures = window.debounceJS.debounce(function() {
     var allPictures = document.querySelectorAll('.picture');
     if (allPictures.length > 0) {
       for (var i = 0; i < allPictures.length; i++) {
         allPictures[i].remove();
       }
     }
-  }
+    return window.debounceJS.debounce;
+  });
 
   function removeAndAddPhotos(parametr) {
     clearPictures();
