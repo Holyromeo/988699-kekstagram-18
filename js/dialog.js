6b5c7333fc809ef278adc3f7d072bdbc8f75fc05
Holyromeo <ramankast@mail.ru>
2019-11-08 18:42:07 +0300

    Задание 15

--- js/dialog.js
+++ js/dialog.js
@@ -7,6 +7,14 @@
   var closeChangeImgBtn = document.getElementById('upload-cancel');
   var resizableImg = document.querySelector('.img-upload__preview');
   var fieldEffectLevel = document.querySelector('.effect-level');
+  var form = document.querySelector('.img-upload__form');
+
+  var blockSuccess = document.querySelector('main');
+  var successTemplate = document.querySelector('#success').content.querySelector('.success');
+  var closeBtnSuccess = document.querySelector('#success').content.querySelector('.success__button');
+  var blockForError = document.querySelector('main');
+  var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+
 
   window.ESC_KEYCODE = ESC_KEYCODE;
   window.hiddenClassAdd = hiddenClassAdd();
@@ -15,6 +23,12 @@
   function onChangeImgEscPress(evt) {
     if (evt.keyCode === ESC_KEYCODE) {
       closeChangeImg();
+    }
+  }
+
+  function closeSuccessMessge(evt) {
+    if (evt.keyCode === window.ESC_KEYCODE) {
+      successTemplate.remove();
     }
   }
 
@@ -41,5 +55,26 @@
   closeChangeImgBtn.addEventListener('click', function () {
     closeChangeImg();
     resizableImg.removeAttribute('style');
+  });
+
+  function onSuccessUpload() {
+    changeImg.classList.add('hidden');
+    blockSuccess.appendChild(successTemplate);
+    document.addEventListener('click', function () {
+      successTemplate.remove();
+    });
+    document.addEventListener('keydown', closeSuccessMessge);
+    closeBtnSuccess.focus();
+    closeBtnSuccess.addEventListener('keydown', closeSuccessMessge);
+  }
+
+  function onErrorUpload(message) {
+    errorTemplate.querySelector('.error__title').textContent = message;
+    blockForError.appendChild(errorTemplate);
+  }
+
+  form.addEventListener('submit', function (evt) {
+    window.upload('https://js.dump.academy/kekstagram', new FormData(form), onSuccessUpload, onErrorUpload);
+    evt.preventDefault();
   });
 })();
