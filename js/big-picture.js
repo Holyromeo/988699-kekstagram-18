e4432516156c15cfcb11f0042de658026cc8d222
Holyromeo <ramankast@mail.ru>
2019-11-13 21:14:29 +0300

    try

--- js/big-picture.js
+++ js/big-picture.js
@@ -5,7 +5,8 @@
   var bigPictureCancel = document.querySelector('.big-picture__cancel');
   var commentsList = document.querySelector('.social__comments');
   var commentItem = document.querySelector('.social__comment');
-  // var buttonLoadComment = document.querySelector('.comments-loader');
+  var buttonLoadComment = document.querySelector('.comments-loader');
+  // var clickPicture = document.querySelector('.picture__img');
 
   function getCommentItem(comment) {
     var createCommentItem = commentItem.cloneNode(true);
@@ -18,8 +19,14 @@
   function createSocialCommentsList(comment) {
     var socialCommentsList = document.createDocumentFragment();
     comment.forEach(function (comments) {
-      socialCommentsList.appendChild(getCommentItem(comments));
+      if (socialCommentsList.childNodes.length < 5) {
+        socialCommentsList.appendChild(getCommentItem(comments));
+      }
+      if (socialCommentsList.childNodes.length > 5 || socialCommentsList.childNodes.length < 10) {
+        buttonLoadComment.classList.remove('visually-hidden');
+      }
     });
+
     commentsList.innerHTML = '';
     commentsList.appendChild(socialCommentsList);
   }
@@ -33,7 +40,9 @@
     bigPicture.querySelector('.comments-count').textContent = picture.comments.length;
     bigPicture.querySelector('.social__caption').textContent = picture.description;
 
+
     createSocialCommentsList(picture.comments);
+
 
     bigPicture.querySelector('.social__comment-count').classList.add('visually-hidden');
     bigPicture.querySelector('.social__comments-loader').classList.add('visually-hidden');
@@ -49,6 +58,7 @@
   function closingBigPicture() {
     bigPicture.classList.add('hidden');
   }
+
   window.bigPicture = {
     showBigPicture: showBigPicture
   };
