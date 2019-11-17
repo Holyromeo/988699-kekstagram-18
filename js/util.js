5028f28106271f4084892b2c19eea3429e401997
Holyromeo <ramankast@mail.ru>
2019-11-17 17:31:03 +0300

    немного причесал 2

--- js/util.js
+++ js/util.js
@@ -5,7 +5,6 @@
   var ENTER_KEYCODE = 13;
   var fieldEffectLevel = document.querySelector('.effect-level');
   var originalEffect = document.getElementById('effect-none');
-  var fieldEffectLevel = document.querySelector('.effect-level');
   var resizableImg = document.querySelector('.img-upload__preview');
 
   var hiddenClassAdd = function () {
@@ -16,7 +15,6 @@
   window.ENTER_KEYCODE = ENTER_KEYCODE;
   window.fieldEffectLevel = fieldEffectLevel;
   window.originalEffect = originalEffect;
-  window.fieldEffectLevel = fieldEffectLevel;
   window.resizableImg = resizableImg;
   window.util = {
     hiddenClassAdd: hiddenClassAdd
